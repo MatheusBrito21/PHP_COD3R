81075e57e17a94e5e2d599b4ab10408f79588f36
Matheus Brito Biserra <matheus.biserra@globalhitss.com.br>
2024-04-23 13:09:55 -0300

    Teste de alteracao de nomes de arquivos

---
+++ teste_2.php
@@ -0,0 +1,30 @@
+<?php
+//Testando a alteração de nome de arquivo baseado em chave valor
+$pasta = 'C:\docs\Livros';
+
+$arquivo = 'nomes.txt';
+
+$array_nomes = [];
+
+$handle = fopen($arquivo, 'r');
+if ($handle) {
+    while (($linha = fgets($handle)) !== false) {
+        $array_nomes [] = $linha;
+    }
+    fclose($handle);
+} else {
+    echo "Não foi possível abrir o arquivo.";
+}
+
+$nomes_k = [];
+foreach ($array_nomes as $nome){
+    $n= explode(' - ',$nome);
+    // print_r($n);
+
+    $nomes_k[$n[1]] = $n[0];
+}
+
+print_r($nomes_k);
+
+
+
