3816cff7bdcb5bb7f6a658535f78e42bc19d1028
matheus.biserra <matheus.biserra@globalhitss.com.br>
2024-04-26 18:40:54 -0300

    Teste de fotos Ok

--- teste_2.php
+++ teste_2.php
@@ -21,7 +21,7 @@ foreach ($array_nomes as $nome){
     $n= explode(' - ',$nome);
     // print_r($n);
 
-    $nomes_k[$n[1]] = $n[0];
+    $nomes_k[trim($n[1])] = trim($n[0]);
 }
 
 print_r($nomes_k);
